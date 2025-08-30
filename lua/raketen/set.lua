4b21b878d6751db183c34707bdfe4b0e0c489957
pyrist <thomas.jurczyk-q88@rub.de>
2025-08-30 16:11:36 +0200

    harpoon

--- lua/raketen/set.lua
+++ lua/raketen/set.lua
@@ -1,21 +1,21 @@
 vim.cmd.colorscheme("tokyonight-day")
- 
+
 vim.opt.clipboard = 'unnamedplus' -- use system keyboard for yank
 
 vim.opt.nu = true                 -- set line numbers -- set line numbers
 vim.opt.relativenumber = true     -- use relative line numbers
- 
+
 -- set tab size to 2 spaces
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
- 
+
 vim.opt.wrap = false
- 
+
 vim.opt.incsearch = true -- incremental search
- 
+
 vim.opt.termguicolors = true
 
 vim.api.nvim_create_autocmd("BufWritePre", {
@@ -27,21 +27,21 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 
 vim.opt.clipboard = "unnamedplus"
 vim.g.clipboard = {
-    name = "wsl",
-    copy = {
-        ["+"] = "clip.exe",
-        ["*"] = "clip.exe",
-    },
-    paste = {
-        ["+"] = "powershell.exe Get-Clipboard",
-        ["*"] = "powershell.exe Get-Clipboard",
-    },
-    cache_enabled = 0,
+  name = "wsl",
+  copy = {
+    ["+"] = "clip.exe",
+    ["*"] = "clip.exe",
+  },
+  paste = {
+    ["+"] = "powershell.exe Get-Clipboard",
+    ["*"] = "powershell.exe Get-Clipboard",
+  },
+  cache_enabled = 0,
 }
 
 vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(event)
-    local opts = {buffer = event.buf}
+    local opts = { buffer = event.buf }
     vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
     vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
     vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
@@ -50,7 +50,26 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
     vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
     vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-    vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+    vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
     vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
   end,
 })
+
+local harpoon = require("harpoon")
+
+-- HARPOON Setup
+-- REQUIRED
+harpoon:setup()
+-- REQUIRED
+
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
